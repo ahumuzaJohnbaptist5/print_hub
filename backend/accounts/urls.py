86a85294e874ed6bb1448a7e173e899cbdc4a73d
Ahumuza John Baptist <262724420+ahumuzaJohnbaptist5@users.noreply.github.com>
2026-06-30 15:21:46 +0300

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,11 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path('verification-sent/', views.verification_sent_view, name='verification_sent'),
-    path('verify/<uuid:token>/', views.verify_email_view, name='verify_email'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    
-    # Add this line for the Profile page
-    path('profile/', views.profile_view, name='profile'), 
+    path('profile/', views.profile_view, name='profile'),
 ]
