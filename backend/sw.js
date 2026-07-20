9478dc0f523b8a99789bea7ad5dc2df27033ab6f
kevin-password <2024ambchb085f@kab.ac.ug>
2026-07-20 14:14:45 +0300

    Add service worker for caching assets

---
+++ backend/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'printhub-v2';
+const urlsToCache = [
+    '/',
+    '/static/manifest.json',
+    '/static/icons/icon.svg',
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
