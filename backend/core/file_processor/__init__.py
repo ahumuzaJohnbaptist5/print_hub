276c0ce4bf194eb2ffa2338e18a5c52aab8c401c
kevin <2024ambchb085f@kab.ac.ug>
2026-07-25 16:36:14 +0300

    Initialize file_processor module with imports

---
+++ backend/core/file_processor/__init__.py
@@ -0,0 +1,7 @@
+# file_processor/__init__.py
+from .processors import FileProcessor
+from .preview import PreviewGenerator
+from .validators import FileValidator
+from .converters import FileConverter
+
+__all__ = ['FileProcessor', 'PreviewGenerator', 'FileValidator', 'FileConverter']
