a8c44fc082a9ee46dfedfec545f3e4ecb03df1e3
kevin <2024ambchb085f@kab.ac.ug>
2026-07-26 06:19:54 +0300

    Clear all imports in orders __init__.py
    Removed all import statements from __init__.py

--- backend/orders/__init__.py
+++ backend/orders/__init__.py
@@ -1,8 +1,1 @@
-# orders/views/__init__.py
-from .client_views import *
-from .admin_views import *
-from .agent_views import *
-from .api_views import *
-from .live_board_views import *
-from .file_views import *
-from .helpers import *
+
