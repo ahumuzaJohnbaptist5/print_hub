b558e6472d0bb1a8a6063ae6751a9808f8c8cae8
kevin <2024ambchb085f@kab.ac.ug>
2026-07-28 16:05:23 +0300

    Add signal to create referral code for new users
    Auto-generates a referral code for new users upon creation.

---
+++ backend/referrals/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from .models import ReferralCode
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def create_referral_code(sender, instance, created, **kwargs):
+    """Auto-generate referral code when a new user is created"""
+    if created:
+        ReferralCode.get_or_create_for_user(instance)
