71c0a89731a2c2f0ab75fc0b49070643b7247543
kevin <2024ambchb085f@kab.ac.ug>
2026-08-05 12:38:00 +0300

    Delete backend/whatsapp_bot directory

--- backend/whatsapp_bot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('webhook/', views.webhook_view, name='whatsapp_webhook'),
-    path('health/', views.health_check, name='whatsapp_health'),
-]
